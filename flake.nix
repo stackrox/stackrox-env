e0d758f82f38f21a269031ca4461fda9b1a43a4f
Stephan Heßelmann <shesselm@redhat.com>
2022-01-16 16:17:29 +0100

    init

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Stackrox development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    nixpkgs-go-1_17_3.url = "github:nixos/nixpkgs/5e15d5da4abb74f0dd76967044735c70e94c5af1";
+    nixpkgs-rocksdb-6_15_5.url = "github:nixos/nixpkgs/a765beccb52f30a30fee313fbae483693ffe200d";
+    openshift.url = "github:stehessel/oc";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, nixpkgs-go-1_17_3, nixpkgs-rocksdb-6_15_5, openshift, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        pkgs-go = nixpkgs-go-1_17_3.legacyPackages.${system};
+        pkgs-oc = openshift.defaultPackage.${system};
+        pkgs-rocksdb = nixpkgs-rocksdb-6_15_5.legacyPackages.${system};
+
+        apps = [ pkgs-oc ];
+        deps = [ pkgs.darwin.apple_sdk.frameworks.Security pkgs-go.go_1_17 pkgs-rocksdb.rocksdb ];
+        kubernetes = [ pkgs.kubectl pkgs.kubectx pkgs.kubernetes-helm ];
+        ui = [ pkgs.nodejs pkgs.yarn ];
+        utils = [ pkgs.bats pkgs.gcc pkgs.gnumake pkgs.jq pkgs.wget pkgs.yq-go ];
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = apps ++ deps ++ kubernetes ++ ui ++ utils;
+        };
+      }
+    );
+}
