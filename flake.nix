7c6daee5fc97b454f410220dd0b021aa746b355b
Stephan Heßelmann <shesselm@redhat.com>
2022-01-19 12:21:32 +0100

    remove golang 1.17.3 pin due to test failures

--- flake.nix
+++ flake.nix
@@ -3,18 +3,16 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
-    nixpkgs-go-1_17_3.url = "github:nixos/nixpkgs/5e15d5da4abb74f0dd76967044735c70e94c5af1";
     nixpkgs-rocksdb-6_15_5.url = "github:nixos/nixpkgs/a765beccb52f30a30fee313fbae483693ffe200d";
     openshift.url = "github:stehessel/oc";
     openshift.inputs.nixpkgs.follows = "nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, nixpkgs-go-1_17_3, nixpkgs-rocksdb-6_15_5, openshift, flake-utils }:
+  outputs = { self, nixpkgs, nixpkgs-rocksdb-6_15_5, openshift, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        pkgs-go = nixpkgs-go-1_17_3.legacyPackages.${system};
         pkgs-oc = openshift.defaultPackage.${system};
         pkgs-rocksdb = nixpkgs-rocksdb-6_15_5.legacyPackages.${system};
 
@@ -32,7 +30,7 @@
           pkgs.yq-go
         ];
         deps = [
-          pkgs-go.go_1_17
+          pkgs.go_1_17
           pkgs-rocksdb.rocksdb
           pkgs.jdk11
         ];
