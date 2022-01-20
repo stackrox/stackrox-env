25b663085e4980869c0404748bcf9596c7060935
Stephan Heßelmann <shesselm@redhat.com>
2022-01-20 22:14:50 +0100

    add openshift overlay

--- flake.nix
+++ flake.nix
@@ -4,51 +4,57 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
     nixpkgs-rocksdb-6_15_5.url = "github:nixos/nixpkgs/a765beccb52f30a30fee313fbae483693ffe200d";
-    openshift.url = "github:stehessel/oc";
-    openshift.inputs.nixpkgs.follows = "nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, nixpkgs-rocksdb-6_15_5, openshift, flake-utils }:
+  outputs = { self, nixpkgs, nixpkgs-rocksdb-6_15_5, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
-        pkgs-oc = openshift.defaultPackage.${system};
-        pkgs-rocksdb = nixpkgs-rocksdb-6_15_5.legacyPackages.${system};
+        openshift-overlay =
+          prev: final: {
+            openshift = final.openshift.overrideAttrs (
+              prev: {
+                nativeBuildInputs = prev.nativeBuildInputs ++ [ final.ncurses ];
+              }
+            );
+          };
+        pkgs = import nixpkgs
+          {
+            inherit system;
+            overlays =
+              if builtins.elem "${system}" nixpkgs.lib.platforms.darwin
+              then
+                [ openshift-overlay ]
+              else [ ];
+          };
+        pkgs-rocksdb = import nixpkgs-rocksdb-6_15_5 { inherit system; };
 
-        cli = [
+        common = [
+          pkgs-rocksdb.rocksdb
           pkgs.bats
           pkgs.gcc
           pkgs.gnumake
+          pkgs.go_1_17
           pkgs.gradle
+          pkgs.jdk11
           pkgs.jq
           pkgs.kubectl
           pkgs.kubectx
           pkgs.kubernetes-helm
+          pkgs.nodejs
+          pkgs.openshift
           pkgs.wget
+          pkgs.yarn
           pkgs.yq-go
         ];
-        deps = [
-          pkgs.go_1_17
-          pkgs-rocksdb.rocksdb
-          pkgs.jdk11
-        ];
-        linux =
-          if builtins.elem "${system}" pkgs.lib.platforms.linux
-          then [ pkgs.openshift ]
-          else [ ];
-        macos =
+        darwin =
           if builtins.elem "${system}" pkgs.lib.platforms.darwin
-          then [ pkgs-oc pkgs.darwin.apple_sdk.frameworks.Security ]
+          then [ pkgs.darwin.apple_sdk.frameworks.Security ]
           else [ ];
-        ui = [
-          pkgs.nodejs
-          pkgs.yarn
-        ];
       in
       {
         devShell = pkgs.mkShell {
-          buildInputs = cli ++ deps ++ linux ++ macos ++ ui;
+          buildInputs = common ++ darwin;
         };
       }
     );
