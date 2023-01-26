2af308e152b440c7ae5764b34124834446a54583
Tomasz Janiszewski <janiszt@gmail.com>
2023-01-26 16:24:16 +0100

    Docker image output

--- flake.nix
+++ flake.nix
@@ -24,45 +24,61 @@
           ps.pyyaml
         ];
         stackrox-python = pkgs.python3.withPackages python-packages;
+        default-packages = [
+          # stackrox/stackrox
+          pkgs-rocksdb.rocksdb
+          pkgs.bats
+          pkgs.gettext # Needed for `envsubst`
+          pkgs.google-cloud-sdk
+          pkgs.gradle
+          pkgs.jdk11
+          pkgs.nodejs
+          pkgs.yarn
+
+          # stackrox/acs-fleet-manager
+          pkgs.aws-vault
+          pkgs.awscli2
+          pkgs.chamber
+          pkgs.krb5 # Dependency of aws-saml.py
+          pkgs.pre-commit
+
+          # openshift
+          pkgs.ocm
+          pkgs.openshift
+
+          # misc
+          pkgs.bashInteractive
+          pkgs.bfg-repo-cleaner
+          pkgs.cachix
+          pkgs.gcc
+          pkgs.gnumake
+          pkgs.go_1_18
+          pkgs.jq
+          pkgs.kubectl
+          pkgs.kubectx
+          pkgs.kubernetes-helm
+          pkgs.wget
+          pkgs.yq-go
+          stackrox-python
+        ];
+        dockerImage = pkgs.dockerTools.buildImage {
+          name = "stackrox-test";
+          copyToRoot = pkgs.buildEnv {
+            name = "image-root";
+
+            paths = default-packages;
+            pathsToLink = [ "/bin" ];
+          };
+          config = { Cmd = [ "${pkgs.bashInteractive}/bin/bash" ]; };
+        };
       in
       {
+        packages = {
+          docker = dockerImage;
+        };
+        defaultPackage = dockerImage;
         devShell = pkgs.mkShell {
-          buildInputs = [
-            # stackrox/stackrox
-            pkgs-rocksdb.rocksdb
-            pkgs.bats
-            pkgs.gettext # Needed for `envsubst`
-            pkgs.google-cloud-sdk
-            pkgs.gradle
-            pkgs.jdk11
-            pkgs.nodejs
-            pkgs.yarn
-
-            # stackrox/acs-fleet-manager
-            pkgs.aws-vault
-            pkgs.awscli2
-            pkgs.chamber
-            pkgs.krb5 # Dependency of aws-saml.py
-            pkgs.pre-commit
-
-            # openshift
-            pkgs.ocm
-            pkgs.openshift
-
-            # misc
-            pkgs.bfg-repo-cleaner
-            pkgs.cachix
-            pkgs.gcc
-            pkgs.gnumake
-            pkgs.go_1_18
-            pkgs.jq
-            pkgs.kubectl
-            pkgs.kubectx
-            pkgs.kubernetes-helm
-            pkgs.wget
-            pkgs.yq-go
-            stackrox-python
-          ] ++ darwin-pkgs;
+          buildInputs = default-packages ++ darwin-pkgs;
         };
       }
     );
