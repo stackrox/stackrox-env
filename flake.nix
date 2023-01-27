16645d92a5630947487ec5cc2cc4dea9ffa66975
Tomasz Janiszewski <janiszt@gmail.com>
2023-01-27 12:52:50 +0100

    Add required packages

--- flake.nix
+++ flake.nix
@@ -12,6 +12,7 @@
       let
         pkgs = import nixpkgs { inherit system; };
         pkgs-rocksdb = import nixpkgs-rocksdb-6_15_5 { inherit system; };
+        inherit (pkgs) lib;
         darwin-pkgs =
           if pkgs.stdenv.isDarwin then [
             pkgs.colima
@@ -47,7 +48,6 @@
           pkgs.openshift
 
           # misc
-          pkgs.bashInteractive
           pkgs.bfg-repo-cleaner
           pkgs.cachix
           pkgs.gcc
@@ -61,15 +61,53 @@
           pkgs.yq-go
           stackrox-python
         ];
-        dockerImage = pkgs.dockerTools.buildImage {
-          name = "stackrox-test";
-          copyToRoot = pkgs.buildEnv {
-            name = "image-root";
 
-            paths = default-packages;
-            pathsToLink = [ "/bin" ];
+        # Dynamic files in the filesystem root of the base image
+        dynamicRootFiles = pkgs.runCommandNoCC "dynamic-root-files" {} ''
+          mkdir -p $out/run $out/usr/bin $out/bin $out/lib64
+          ln -s ${pkgs.coreutils}/bin/env $out/usr/bin/env
+          ln -s ${pkgs.bashInteractive}/bin/sh $out/bin/sh
+          # So that this image can be used as a GitHub Action container directly
+          # Needed because it calls its own (non-nix-patched) node binary which uses
+          # this dynamic linker path. See also the LD_LIBRARY_PATH assignment below,
+          # which provides the necessary libraries for that binary
+          ln -s ${pkgs.glibc}/lib/ld-linux-x86-64.so.2 $out/lib64/ld-linux-x86-64.so.2
+          # Fix for go package
+          ln -s ${pkgs.go_1_18}/bin/go $out/bin/go
+          mkdir $out/tmp
+        '';
+
+        gha-packages = [
+          pkgs.cacert
+          pkgs.coreutils
+          pkgs.bashInteractive
+          pkgs.findutils
+          pkgs.gnugrep
+          pkgs.gnused
+          pkgs.gitMinimal
+          dynamicRootFiles
+        ];
+
+        dockerImage = pkgs.dockerTools.buildLayeredImage {
+          name = "quay.io/tjanisze/stackrox-test";
+          tag = "latest";
+          contents =  default-packages ++ gha-packages;
+          maxLayers = 125;
+          config = {
+            Cmd = [ "${pkgs.bashInteractive}/bin/bash" ];
+            Env = [
+              #
+              "TMPDIR=/tmp"
+              # Needed for git.
+              "NIX_SSL_CERT_FILE=/etc/ssl/certs/ca-bundle.crt"
+              # https://github.com/teamniteo/nix-docker-base/blob/fefa/image.nix#L135-L139
+              # By default, the linker added in dynamicRootFiles can only find glibc
+              # libraries, but the node binary from the GitHub Actions runner also
+              # depends on libstdc++.so.6, which is glibc/stdenv. Using LD_LIBRARY_PATH
+              # is the easiest way to inject this dependency
+              "LD_LIBRARY_PATH=${lib.makeLibraryPath [ pkgs.stdenv.cc.cc ]}"
+            ];
           };
-          config = { Cmd = [ "${pkgs.bashInteractive}/bin/bash" ]; };
         };
       in
       {
